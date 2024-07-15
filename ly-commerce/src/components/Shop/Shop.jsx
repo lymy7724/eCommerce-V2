bc00abb0ce9c35beaff613ae45f6ef279f29494a
Ly My Y <lymy7724@gmail.com>
2024-07-15 09:18:46 -0400

    edited home page
    added pictures and edited more of the home page

--- ly-commerce/src/components/Shop/Shop.jsx
+++ ly-commerce/src/components/Shop/Shop.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import mysql from 'mysql2'
 
 export default function Shop() {
+
+  const connection = mysql.createConnection({
+    host: "sql5.freesqldatabase.com",
+    user: "sql5719789",
+    password: "pGFUCcYpSy",
+    database: "sql5719789",
+  });
+  
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Database is connected!!");
+  });
+
+  connection.query(
+    "SELECT * FROM products",
+    function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    }
+  );
+
   return (
     <div>
       <p>shop</p>
