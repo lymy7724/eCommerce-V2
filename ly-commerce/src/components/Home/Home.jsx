46138d92071fce069da7788c71876afbed45468b
Ly My Y <lymy7724@gmail.com>
2024-07-14 21:45:09 -0400

    first edits
    set up the nav links

---
+++ ly-commerce/src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+    <div class="greetings">
+        <h3>Welcome to Crochetopia</h3>
+        <p>Step into a world of handmade charm and crochet wonders. Each stitch is
+            crafted with
+            love and care, just for
+            you. Explore our cozy creations and find the perfect piece to add warmth and style to your life. Happy
+            browsing!
+        </p>
+    </div>
+    </div>
+  )
+}
